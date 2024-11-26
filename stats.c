ac12309b36b9838e8e14013b9b1096214f910c90
peleja23 <pedro_peleja@hotmail.com>
2024-11-26 15:40:30 +0000

    v3.1

--- stats.c
+++ stats.c
@@ -1,10 +1,15 @@
 #include "my_stats.h"
 
 int main(int argc, char *argv[]) {
-
+    
     exec_sort(argv[1], argv[2]);
     get_stats(argv[1], argv[2]);
-    write_in_file();
 
+    if(strcmp("-f",argv[3]) == 0){
+        write_in_file();
+    }
+    if(strcmp("-d",argv[3]) == 0){
+        write_in_file();
+    }
     return 0;
 }
